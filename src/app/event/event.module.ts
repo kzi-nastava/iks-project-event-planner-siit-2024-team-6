fc98d57ab3b39f758af5c55e4dc46b53f47f5006
DusicaPesic <dusicapesich@gmail.com>
2024-11-16 23:03:04 +0100

    [ADD] event slider component for top events

--- src/app/event/event.module.ts
+++ src/app/event/event.module.ts
@@ -2,19 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventCardComponent } from './event-card/event-card.component';
 import { EventListComponent } from './event-list/event-list.component';
+import { EventSliderComponent } from './event-slider/event-slider.component';
+import { TopEventCardComponent } from './top-event-card/top-event-card.component';
 
 
 
 @NgModule({
   declarations: [
     EventCardComponent,
-    EventListComponent
+    EventListComponent,
+    EventSliderComponent,
+    TopEventCardComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    EventListComponent
+    EventListComponent,
+    EventSliderComponent
   ]
 })
 export class EventModule { }
