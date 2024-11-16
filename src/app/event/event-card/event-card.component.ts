b7ef830f77da523de3473dc153ba0bf3bb2bf177
DusicaPesic <dusicapesich@gmail.com>
2024-11-16 17:55:12 +0100

    [UPDATE] event card to get input from service

--- src/app/event/event-card/event-card.component.ts
+++ src/app/event/event-card/event-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {Event} from '../model/event.model';
 
 @Component({
   selector: 'app-event-card',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './event-card.component.css'
 })
 export class EventCardComponent {
-
+  @Input() event!: Event;
 }
