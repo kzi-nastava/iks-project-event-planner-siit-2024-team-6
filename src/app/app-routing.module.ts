7e8606986c89f3d8e7dedcd69c44f09a587276ea
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-11-16 15:35:42 +0100

    [ADD] Login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { WinesComponent } from './wine/wines/wines.component';
 import { AddWineComponent } from './wine/add-wine/add-wine.component';
 import { HomeComponent } from './layout/home/home.component';
+import { LoginComponent } from './infrastructure/auth/login/login.component';
 
 const routes: Routes = [
   {path: 'wine', component: WinesComponent},
   {path: 'add', component: AddWineComponent},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { LayoutModule } from './layout/layout.module';
 import { WineModule } from './wine/wine.module';
+import { AuthModule } from './infrastructure/auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { WineModule } from './wine/wine.module';
     BrowserModule,
     AppRoutingModule,
     LayoutModule,
-    WineModule
+    WineModule,
+    AuthModule
   ],
   providers: [
     provideAnimationsAsync()
