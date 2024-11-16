74e64cb56591c285a6d7b158b1dc892d21ed12cb
DusicaPesic <dusicapesich@gmail.com>
2024-11-16 15:07:26 +0100

    [ADD] footer component

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -4,19 +4,24 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MaterialModule } from '../infrastructure/material/material.module';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     NavBarComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
     MaterialModule,
     RouterModule
   ],
-  exports: [NavBarComponent]
+  exports: [
+    FooterComponent,
+    NavBarComponent
+  ]
 })
 export class LayoutModule { }
