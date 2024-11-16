881c33ea877c1d2245a3e650f916f4afe81d4724
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-11-16 17:20:34 +0100

    [ADD] Login button, [EDIT] Design

--- src/app/infrastructure/auth/auth.module.ts
+++ src/app/infrastructure/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -11,7 +12,8 @@ import { RegistrationComponent } from './registration/registration.component';
     RegistrationComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     LoginComponent,
