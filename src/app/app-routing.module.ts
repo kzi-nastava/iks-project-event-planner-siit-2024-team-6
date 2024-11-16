ba86b024d1fb93deb7f56fea220c840bc3cd396c
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-11-16 16:08:23 +0100

    [EDIT] Configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { WinesComponent } from './wine/wines/wines.component';
 import { AddWineComponent } from './wine/add-wine/add-wine.component';
 import { HomeComponent } from './layout/home/home.component';
 import { LoginComponent } from './infrastructure/auth/login/login.component';
+import { RegistrationComponent } from './infrastructure/auth/registration/registration.component';
 
 const routes: Routes = [
   {path: 'wine', component: WinesComponent},
   {path: 'add', component: AddWineComponent},
   {path: 'home', component: HomeComponent},
-  { path: 'login', component: LoginComponent }
+  {path: 'login', component: LoginComponent},
+  {path: 'registration', component: RegistrationComponent}
 ];
 
 @NgModule({
