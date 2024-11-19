ce5015603d66d32cfb327fa15d01410b40bce366
DusicaPesic <dusicapesich@gmail.com>
2024-11-19 23:57:47 +0100

    [ADD] offers slider for displaying top 5 offers

---
+++ src/app/offer/offer-slider/offer-slider.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Offer } from '../model/offer.model';
+import { OfferService } from '../offer.service';
+
+@Component({
+  selector: 'app-offer-slider',
+  templateUrl: './offer-slider.component.html',
+  styleUrl: './offer-slider.component.css'
+})
+export class OfferSliderComponent implements OnInit{
+  offers: Offer[] = [];
+  currentPosition: number = 0;
+
+  constructor(private offerService: OfferService) {}
+
+  ngOnInit(): void{
+    this.offers = this.offerService.getAll();
+  }
+
+  slideLeft(): void {
+    const content = document.querySelector('.slider-content') as HTMLElement;
+    const cardWidth = content.offsetWidth;
+    this.currentPosition = Math.min(this.currentPosition + cardWidth, 0);
+    content.style.transform = `translateX(${this.currentPosition}px)`;
+  }
+  slideRight(): void {
+    const content = document.querySelector('.slider-content') as HTMLElement;
+    const cardWidth = content.offsetWidth;
+    const maxScroll = -(content.scrollWidth - content.offsetWidth);
+    this.currentPosition = Math.max(this.currentPosition - cardWidth, maxScroll);
+    content.style.transform = `translateX(${this.currentPosition}px)`;
+  }
+}
+
