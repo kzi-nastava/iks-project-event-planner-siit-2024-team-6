49bdf588f7682f614f5225979db2fa3c0f3531d5
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2024-11-20 21:41:01 +0100

    [ADD] component for list of providers services

---
+++ src/app/offer/provider-service-list/provider-service-list.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProviderServiceListComponent } from './provider-service-list.component';
+
+describe('ProviderServiceListComponent', () => {
+  let component: ProviderServiceListComponent;
+  let fixture: ComponentFixture<ProviderServiceListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ProviderServiceListComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ProviderServiceListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
