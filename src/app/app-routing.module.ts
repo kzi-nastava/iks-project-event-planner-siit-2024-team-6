0c9e706113b1ec576f2ec1fb3f9c84f79fdf0d9a
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-11-20 22:29:28 +0100

    [BUG] Navigation works properly bug solved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EventsViewComponent } from './event/events-view/events-view.component';
 import { OffersViewComponent } from './offer/offers-view/offers-view.component';
+import { HomeComponent } from './layout/home/home.component';
+import { LoginComponent } from './infrastructure/auth/login/login.component';
+import { RegistrationComponent } from './infrastructure/auth/registration/registration.component';
 const routes: Routes = [
   { path: 'events', component: EventsViewComponent },
   { path: 'offers', component: OffersViewComponent },
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'registration', component: RegistrationComponent},
   { path: '', redirectTo: '/events', pathMatch: 'full' }
 
 ];
