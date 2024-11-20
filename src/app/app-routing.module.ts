f51403b497a17057e32ec18a26ea5d9b725adf56
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2024-11-20 21:50:00 +0100

    [ADD] routing for provider's services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EventsViewComponent } from './event/events-view/events-view.component';
 import { OffersViewComponent } from './offer/offers-view/offers-view.component';
+import { ProviderServiceViewComponent } from './offer/provider-service-view/provider-service-view.component';
+import { ProviderServiceFormComponent } from './offer/provider-service-form/provider-service-form.component';
+
 const routes: Routes = [
   { path: 'events', component: EventsViewComponent },
   { path: 'offers', component: OffersViewComponent },
+  { path: 'my-services', component: ProviderServiceViewComponent },
   { path: '', redirectTo: '/events', pathMatch: 'full' }
-
 ];
 
 @NgModule({
