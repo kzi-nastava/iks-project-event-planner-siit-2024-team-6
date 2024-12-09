949bc0ccfea18d50a5e01f65f36b6dfaf5358a35
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2024-12-09 23:50:54 +0100

    [ADD] pagination for the event list and integrate the frontend with the backend

--- src/app/event/event.service.ts
+++ src/app/event/event.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Event } from './model/event.model';
 import { Status } from '../enums/status.enum';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import {environment} from '../../env/environment';
+import {Observable} from 'rxjs';
+import {PagedResponse} from '../shared/model/paged-response.model';
 const EVENTS: Event[] = [
   {
     name: 'Yoga Retreat',
     description: 'A peaceful and rejuvenating retreat focusing on yoga, meditation, and mindfulness.',
-    location: 'Mountain Bliss Resort, Aspen, Colorado',
+    place: 'Mountain Bliss Resort, Aspen, Colorado',
     maxParticipants: 30,
     minParticipants: 5,
     public: true,
     disabled: false,
     date: new Date('2024-09-20'),
-    photo: 'https://images.squarespace-cdn.com/content/v1/590e5eff20099e49cdc1048d/1506078288556-5AXBZX4RZ2DCOJ6ECUW0/UBU_660.jpg',
+    photos: ['https://images.squarespace-cdn.com/content/v1/590e5eff20099e49cdc1048d/1506078288556-5AXBZX4RZ2DCOJ6ECUW0/UBU_660.jpg'],
     activities: [
       {
         name: 'Morning Yoga Session',
@@ -56,13 +60,13 @@ const EVENTS: Event[] = [
   {
     name: 'Tech Conference',
     description: 'An annual technology conference featuring top speakers.',
-    location: 'Tech Convention Center, Auditorium A, San Francisco, USA',
+    place: 'Tech Convention Center, Auditorium A, San Francisco, USA',
     maxParticipants: 500,
     minParticipants: 50,
     public: true,
     disabled: false,
     date: new Date('2024-07-10'),
-    photo: "https://www.thebrewery.co.uk/wp-content/uploads/2020/10/Theatre-style-7.jpg",
+    photos: ["https://www.thebrewery.co.uk/wp-content/uploads/2020/10/Theatre-style-7.jpg"],
     activities: [
       {
         name: 'Keynote Speech',
@@ -97,13 +101,13 @@ const EVENTS: Event[] = [
   {
     name: 'Outdoor Concert',
     description: 'A night of music and fun under the stars.',
-    location: 'Central Park, Open Stage, New York, USA',
+    place: 'Central Park, Open Stage, New York, USA',
     maxParticipants: 1000,
     minParticipants: 100,
     public: true,
     disabled: false,
     date: new Date('2024-08-20'),
-    photo: 'https://images.stockcake.com/public/b/1/e/b1efd91e-0c22-4e79-8d5b-6618f8d946ff_large/outdoor-concert-crowd-stockcake.jpg',
+    photos: ['https://images.stockcake.com/public/b/1/e/b1efd91e-0c22-4e79-8d5b-6618f8d946ff_large/outdoor-concert-crowd-stockcake.jpg'],
     activities: [
       {
         name: 'Live Band Performance',
@@ -138,13 +142,13 @@ const EVENTS: Event[] = [
   {
     name: 'Birthday Party',
     description: 'A fun-filled birthday celebration with friends and family.',
-    location: 'Event Hall "Celebrations", Main Street 123, Belgrade, Serbia',
+    place: 'Event Hall "Celebrations", Main Street 123, Belgrade, Serbia',
     maxParticipants: 50,
     minParticipants: 10,
     public: true,
     disabled: false,
     date: new Date('2024-06-15'),
-    photo: 'https://img.freepik.com/premium-photo/friends-celebrating-birthday-party_1280275-114523.jpg',
+    photos: ['https://img.freepik.com/premium-photo/friends-celebrating-birthday-party_1280275-114523.jpg'],
     activities: [
       {
         name: 'Cake Cutting',
@@ -179,13 +183,13 @@ const EVENTS: Event[] = [
   {
     name: 'Painting Workshop: Mastering Watercolors',
     description: 'A creative workshop to explore the art of watercolor painting with expert guidance.',
-    location: 'Art Studio, Downtown Chicago, Illinois',
+    place: 'Art Studio, Downtown Chicago, Illinois',
     maxParticipants: 20,
     minParticipants: 5,
     public: true,
     disabled: false,
     date: new Date('2024-11-25'),
-    photo: 'https://images.squarespace-cdn.com/content/v1/5804f37703596e417ce39f7f/7c4f36cb-2106-42c2-83ee-c2fbfb7a9460/Adult+abstract+art+painting+class+Brighton+UK.jpg',
+    photos: ['https://images.squarespace-cdn.com/content/v1/5804f37703596e417ce39f7f/7c4f36cb-2106-42c2-83ee-c2fbfb7a9460/Adult+abstract+art+painting+class+Brighton+UK.jpg'],
     activities: [
       {
         name: 'Introduction to Watercolors',
@@ -238,36 +242,20 @@ const EVENTS: Event[] = [
   providedIn: 'root',
 })
 export class EventService {
-  private eventList: Event[] = [];
 
-  constructor() {
-    for (let eventObj of EVENTS) {
-      const event: Event = {
-        name: eventObj.name,
-        description: eventObj.description,
-        location: eventObj.location,
-        maxParticipants: eventObj.maxParticipants,
-        minParticipants: eventObj.minParticipants,
-        public: eventObj.public,
-        disabled: eventObj.disabled,
-        date: new Date(eventObj.date),
-        photo: eventObj.photo || 'https://via.placeholder.com/300x200',
-        activities: eventObj.activities || [],
-        budget: eventObj.budget || { maxPrices: 0 },
-        categories: eventObj.categories || [],
-        eventType: eventObj.eventType || { name: 'General', description: 'General event type', isDisabled: false },
-        reservedOffers: eventObj.reservedOffers || [],
-        boughtProducts: eventObj.boughtProducts || [],
-      };
-      this.eventList.push(event);
+  private apiUrl = environment.apiHost+`/events/`; 
+  constructor(private httpClient: HttpClient){}
+  
+
+  getAll(pageProperties?: any): Observable<PagedResponse<Event>> {
+    let params = new HttpParams();
+    if(pageProperties){
+      params = params.set('page',pageProperties.page).set('size',pageProperties.pageSize)
     }
-  }  
-
-  getAll(): Event[] {
-    return this.eventList;
+    return this.httpClient.get<PagedResponse<Event>>(this.apiUrl+`all-elements`, {params:params});
   }
 
-  add(event: Event): void {
-    this.eventList.push(event);
+  getTopFive(): Observable<Event[]> {
+    return this.httpClient.get<Event[]>(this.apiUrl+`top-five`);
   }
 }
