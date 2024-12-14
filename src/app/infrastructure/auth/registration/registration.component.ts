696ac0badbc8e1423cbac18fed68194bd0145cfe
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-12-15 00:41:35 +0100

    [FIX] Registration

--- src/app/infrastructure/auth/registration/registration.component.ts
+++ src/app/infrastructure/auth/registration/registration.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
@@ -11,7 +12,7 @@ export class RegistrationComponent {
   registrationForm: FormGroup;
   isProvider: boolean = false;
 
-  constructor(private fb: FormBuilder, private http: HttpClient) {
+  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) {
     this.registrationForm = this.fb.group({
       name: [''],
       lastname: [''],
@@ -40,7 +41,11 @@ export class RegistrationComponent {
     if (this.registrationForm.valid) {
       const formData = this.registrationForm.value;
       this.http.post('/api/users', formData).subscribe({
-        next: (response) => console.log('Registration successful!', response),
+        next: (response) => {
+          console.log('Registration successful!', response);
+          // Редирект при успешной регистрации
+          this.router.navigate(['/login']); // Укажите путь для редиректа
+        },
         error: (error) => console.error('Registration failed.', error),
       });
     } else {
