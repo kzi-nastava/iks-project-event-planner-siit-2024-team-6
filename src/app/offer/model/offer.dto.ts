41f9c12bf477bfda753bb5f8b53b39f33e8677df
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2024-12-21 20:38:06 +0100

    [ADD] offer data transfer object

---
+++ src/app/offer/model/offer.dto.ts
@@ -0,0 +1,27 @@
+import { EventType } from "../../event/model/event.model";
+
+export interface OfferDTO {
+    status: string;
+    name: string;
+    description: string;
+    price: number;
+    sale?: number;
+    photos: string[];
+    isVisible: boolean;
+    isAvailable: boolean;
+    isDeleted: boolean;
+    category: string;
+    categorySuggestion?: any; // Adjust the type based on your frontend needs
+    eventTypes: EventType[];
+    type: string; // "Product" or "Service"
+  
+    // Service-specific fields
+    specifics?: string;
+    minDuration?: number;
+    maxDuration?: number;
+    preciseDuration?: number;
+    latestReservation?: number;
+    latestCancelation?: number;
+    isReservationAutoApproved: boolean;
+  }
+  
