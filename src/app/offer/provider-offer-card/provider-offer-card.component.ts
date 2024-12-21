b6cb9e535621064de023ea836a406da5117053e7
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2024-12-21 20:39:27 +0100

    [UPDATE] display and layout of service card

--- src/app/offer/provider-offer-card/provider-offer-card.component.ts
+++ src/app/offer/provider-offer-card/provider-offer-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Offer } from '../model/offer.model';
+import { Offer, Service } from '../model/offer.model';
 import { Route, Router } from '@angular/router';
 import { OfferService } from '../offer.service';
 @Component({
@@ -8,13 +8,18 @@ import { OfferService } from '../offer.service';
   styleUrl: './provider-offer-card.component.css'
 })
 export class ProviderOfferCardComponent {
-  @Input() offer!: Offer;
+  @Input() offer!: Service;
   constructor(
     private router: Router,
     private dataTransfer: OfferService
   ) {}
-  editService(service: Offer): void {
+  editService(service: Service): void {
+    console.log("SERVICE IN EDIT"+ service.name);
     this.dataTransfer.setService(service);
     this.router.navigate(['/service-edit']);
   }
+  onImageError(event: Event): void {
+    (event.target as HTMLImageElement).src = 'https://via.placeholder.com/300x200.png?text=404+Not+Found';
+  }  
+  
 }
