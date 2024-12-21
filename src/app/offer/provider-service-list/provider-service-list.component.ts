dcdec7107285921dd888d9bbb6d5eb75832b6cea
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2024-12-21 20:39:09 +0100

    [UPDATE] service list component to work with backend

--- src/app/offer/provider-service-list/provider-service-list.component.ts
+++ src/app/offer/provider-service-list/provider-service-list.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { OfferService } from '../offer.service';
 import { Service } from '../model/offer.model';
 import { Router } from '@angular/router';
+import { PageEvent } from '@angular/material/paginator';
+import { PagedResponse } from '../../shared/model/paged-response.model';
 
 @Component({
   selector: 'app-provider-service-list',
@@ -9,17 +11,39 @@ import { Router } from '@angular/router';
   styleUrl: './provider-service-list.component.css'
 })
 export class ProviderServiceListComponent {
-  services:Service [];
+   offers: Service[] = [];
+    pageProperties = {
+      page: 0,
+      pageSize: 8,
+      totalCount: 0,
+      pageSizeOptions: [4, 8, 12]
+   };
 
   isFilterVisible = false;
   constructor(private offerService: OfferService, private router: Router) {}
   isButtonDisabled: boolean = false; // Default to enabled
 
   ngOnInit(): void {
-    this.services = this.offerService.getServices();
-    console.log(this.services);
+    this.getPagedEntities()
+    console.log(this.offers);
     
   }
+  pageChanged(pageEvent: PageEvent){
+      this.pageProperties.page = pageEvent.pageIndex;
+      this.pageProperties.pageSize = pageEvent.pageSize;
+      this.getPagedEntities();
+  }
+  private getPagedEntities(){
+    this.offerService.getAllProviderServices(this.pageProperties).subscribe({next: (response: PagedResponse<Service>)=>{
+      this.offers = response.content;
+      console.log(this.offers[0]);
+      this.pageProperties.totalCount = response.totalElements;
+      console.log(response);
+    
+    },
+    error: (err) => console.error(err),});
+  }
+  
   toggleFilter(): void {
     this.isFilterVisible = !this.isFilterVisible;
     this.isButtonDisabled = this.isFilterVisible;
