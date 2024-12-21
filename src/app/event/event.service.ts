4ca17c1a326d3cdaf117f5f6ace24c1e22b49633
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2024-12-21 20:37:03 +0100

    [ADD] methods for sending requests to endpoints

--- src/app/event/event.service.ts
+++ src/app/event/event.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Event } from './model/event.model';
+import { Event, EventType } from './model/event.model';
 import { Status } from '../enums/status.enum';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import {environment} from '../../env/environment';
@@ -258,4 +258,13 @@ export class EventService {
   getTopFive(): Observable<Event[]> {
     return this.httpClient.get<Event[]>(this.apiUrl+`top-five`);
   }
+
+  getAllNames(): Observable<string[]>{
+      return this.httpClient.get<string[]>(this.apiUrl+"event-types");
+  }
+
+  getEventTypeByName(name: String): Observable<EventType>{
+    return this.httpClient.get<EventType>(`${this.apiUrl}${name}/event-type`);
+  }
+  
 }
