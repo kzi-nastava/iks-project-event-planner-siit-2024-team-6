2babca10234be3fccbff76311688274d2b6f9fd5
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2024-12-25 17:22:19 +0100

    [ADD] components to module

--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -3,20 +3,33 @@ import { CommonModule } from '@angular/common';
 import { AdminCategoryListComponent } from './admin-category-list/admin-category-list.component';
 import { AdminCategoryCardComponent } from './admin-category-card/admin-category-card.component';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { CategoryDialogComponent } from './category-dialog/category-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AdminCategoryListComponent,
-    AdminCategoryCardComponent
+    AdminCategoryCardComponent,
+    CategoryDialogComponent
   ],
   imports: [
     CommonModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    ReactiveFormsModule
   ],
   exports:[
     AdminCategoryListComponent,
-    AdminCategoryCardComponent
+    AdminCategoryCardComponent,
+    CategoryDialogComponent
   ]
 })
 export class CategoryModule { }
