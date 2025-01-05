ed87193426e210f8f77f4d5bbc8c128e52581ca6
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2025-01-06 00:24:38 +0100

    [UPDATE] login and quick registration to prefill email if they are accessed via link

--- src/app/infrastructure/auth/login/login.component.ts
+++ src/app/infrastructure/auth/login/login.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import {AuthService} from "../auth.service";
 import {AuthResponse} from '../model/auth-response.model';
 import {Login} from "../model/login.model";
@@ -14,30 +14,49 @@ import {Login} from "../model/login.model";
 })
 export class LoginComponent {
   loginForm: FormGroup;
+  email: string = '';
+  disableEmail: boolean = false;
 
   constructor(
     private fb: FormBuilder,
     private http: HttpClient,
     private authService: AuthService,
-              private router: Router
+    private router: Router,
+    private route: ActivatedRoute // Inject ActivatedRoute to get query parameters
+
   ) {
     this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
+      email: [{ value: '', disabled: false }, [Validators.required, Validators.email]],
       password: ['', Validators.required],
     });
   }
 
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.email = params['email'] || '';
+      this.disableEmail = params['disableEmail'] === 'true';
+
+      this.loginForm.patchValue({ email: this.email });
+
+      if (this.disableEmail) {
+        this.loginForm.get('email')?.disable();
+      }
+    });
+  }
+
   onLogin(): void {
+    console.log("Email je ",this.email);
+    console.log("pass je  ", this.loginForm.value.password);
     if (this.loginForm.valid) {
       const login: Login = {
-        email: this.loginForm.value.email || "",
+        email: this.loginForm.get('email')?.value || "",
         password: this.loginForm.value.password || ""
       }
       this.authService.login(login).subscribe({
         next: (response: AuthResponse) => {
           localStorage.setItem('user', response.token);
           this.authService.setUser()
-          this.router.navigate(['home'])
+          this.router.navigate(['events'])
         }
       })
     } else {
