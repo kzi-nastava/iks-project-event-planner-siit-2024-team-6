8ba0d54ee37b5f01fc44cfe7bd26b158375d4ca3
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2025-06-06 22:17:20 +0200

    [ADD] comment service

---
+++ src/app/comment/comment.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Reaction } from './model/comment.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+  private apiUrl = 'http://localhost:8080/api/reactions';
+
+  constructor(private http: HttpClient) {}
+
+  getAllComments(): Observable<Reaction[]> {
+    return this.http.get<Reaction[]>(`${this.apiUrl}/pending`);
+  }
+
+  updateCommentStatus(id: number, status: 'approved' | 'deleted'): Observable<any> {
+    return this.http.patch(`${this.apiUrl}/${id}/status`, { status });
+  }
+
+  deleteComment(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+
+  acceptComment(id: number): Observable<Reaction> {
+    return this.http.put<Reaction>(`${this.apiUrl}/${id}/accept`, {});
+  }
+}
