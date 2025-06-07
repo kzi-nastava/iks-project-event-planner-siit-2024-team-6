4ccadb8ff769e4c42d84f86ae7a08d1c8be01bf2
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2025-06-08 01:33:33 +0200

    [ADD] report dialog pops up when report user is clicked in company dialog

--- src/app/report/report.module.ts
+++ src/app/report/report.module.ts
@@ -3,17 +3,33 @@ import { CommonModule } from '@angular/common';
 import { ReportListComponent } from './report-list/report-list.component';
 import { ReportCardComponent } from './report-card/report-card.component';
 import { MatPaginatorModule } from '@angular/material/paginator';
-
+import { ReportDialogComponent } from './report-dialog/report-dialog.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
   declarations: [
     ReportListComponent,
-    ReportCardComponent
+    ReportCardComponent,
+    ReportDialogComponent
   ],
   imports: [
     CommonModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDialogModule,
+    MatButtonModule,
+  ],
+  exports: [
+    ReportDialogComponent
   ]
 })
 export class ReportModule { }
