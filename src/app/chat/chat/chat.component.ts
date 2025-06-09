d7541ec6ebf709efebc40137ea550b81dda47ecb
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2025-06-09 21:31:58 +0200

    [ADD] methods sending requests to backend regarding chat

--- src/app/chat/chat/chat.component.ts
+++ src/app/chat/chat/chat.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chat, Message } from '../model/chat.model';
+import { ActivatedRoute } from '@angular/router';
+import { ChatService } from '../chat.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { NewMessageDTO } from '../../dto/message-dtos';
 
+// chat.component.ts
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.css'
+  styleUrls: ['./chat.component.css']
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit {
+  messages: Message[] = [];
+  newMessage = '';
+  receiverId!: number;
+  receiver?: Chat;
 
+  constructor(
+    private route: ActivatedRoute,
+    private chatService: ChatService,
+    private snackBar: MatSnackBar
+  ) { }
+
+  ngOnInit() {
+    this.receiverId = +this.route.snapshot.paramMap.get('id')!;
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.chatService.getMessages(this.receiverId).subscribe({
+      next: (data) => this.messages = data,
+      error: (err) => {
+        this.snackBar.open('Could not load messages.', 'Close', {
+          duration: 3000,
+          panelClass: ['snackbar-error']
+        });
+      }
+    });
+  }
+
+  sendMessage() {
+    if (this.newMessage.trim().length == 0){
+      return;
+    }
+    const message: NewMessageDTO = {
+      text: this.newMessage
+    };
+    this.chatService.sendMessage(this.receiverId, message).subscribe({
+      next: (messageDto) => {
+        this.messages.push(messageDto);  // Optionally update chat view
+        this.newMessage = '';
+      },
+      error: () => {
+        this.snackBar.open('Failed to send message.', 'Close', {
+          duration: 3000,
+          panelClass: ['snackbar-error']
+        });
+      }
+    });
+  }
 }
