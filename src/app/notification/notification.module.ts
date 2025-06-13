8a4951e76848b565439323fb969d2f21130195a6
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2025-06-14 00:15:28 +0200

    [ADD] management of notifications mute state using toggle button

--- src/app/notification/notification.module.ts
+++ src/app/notification/notification.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotificationListComponent } from './notification-list/notification-list.component';
 import { MatPaginatorModule } from '@angular/material/paginator';
-
+import {MatSlideToggleModule} from '@angular/material/slide-toggle'
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,9 @@ import { MatPaginatorModule } from '@angular/material/paginator';
   ],
   imports: [
     CommonModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatSlideToggleModule,
+    FormsModule
   ]
 })
 export class NotificationModule { }
