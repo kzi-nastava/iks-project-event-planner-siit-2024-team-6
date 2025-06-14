23fb8e70454987017b847a6c31fc2d3775b4f91b
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2025-06-14 16:51:52 +0200

    [ADD] implementation of updating a pice list item

--- src/app/offer/price-list-table/price-list-table.component.ts
+++ src/app/offer/price-list-table/price-list-table.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { PriceListItem } from '../model/offer.model';
+import { PriceEditDialogComponent } from '../price-edit-dialog/price-edit-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+import { OfferService } from '../offer.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-price-list-table',
@@ -12,6 +17,8 @@ export class PriceListTableComponent implements OnInit {
 
   dataSource: PriceListItem[] = [];
 
+  constructor(private dialog: MatDialog, private offerService: OfferService, private snackBar: MatSnackBar) { }
+
   ngOnInit(): void {
     this.dataSource = this.items;
   }
@@ -22,5 +29,46 @@ export class PriceListTableComponent implements OnInit {
 
   edit(item: PriceListItem): void {
     console.log('Editing:', item);
+    const dialogRef = this.dialog.open(PriceEditDialogComponent, {
+      width: '400px',
+      data: {
+        offerPrice: item.offerPrice,
+        offerDiscountPrice: item.offerDiscountPrice
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.offerService.updatePrice(item.offerId, {
+          price: result.offerPrice,
+          salePrice: result.offerDiscountPrice
+        }).subscribe({
+          next: (updatedItem: PriceListItem) => {
+            const index = this.dataSource.findIndex(i => i.offerId === updatedItem.offerId);
+            if (index > -1) {
+              this.dataSource[index] = updatedItem;
+              this.dataSource = [...this.dataSource]; // trigger change detection
+            }
+            this.snackBar.open('Price updated successfully', 'Close', {
+              duration: 3000
+            });
+          },
+          error: (error: HttpErrorResponse) => {
+            if (error.status === 400) {
+              this.snackBar.open('Discounted price must be lower than the original', 'Close', {
+                duration: 4000,
+                panelClass: ['snackbar-error']
+              });
+            } else {
+              this.snackBar.open('Failed to update price. Try again later.', 'Close', {
+                duration: 4000,
+                panelClass: ['snackbar-error']
+              });
+            }
+          }
+        });
+
+      }
+    });
   }
 }
